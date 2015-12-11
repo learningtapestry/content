50a157d94cfdd6a957a0ea8a1b4a1eba5c2c4056
RM Saksida <rm@saksida.com.br>
2015-12-11 20:30:32 -0200

    Add seeds for review statuses

---
+++ db/seeds/document_statuses.seeds.rb
@@ -0,0 +1,3 @@
+DocumentStatus.find_or_create_by!(value: :unpublished)
+DocumentStatus.find_or_create_by!(value: :published)
+DocumentStatus.find_or_create_by!(value: :hidden)
--- db/seeds/review_statuses.seeds.rb
+++ db/seeds/review_statuses.seeds.rb
@@ -1,3 +1,3 @@
-ReviewStatus.find_or_create_by(value: :system)
-ReviewStatus.find_or_create_by(value: :reviewed)
-ReviewStatus.find_or_create_by(value: :not_reviewed)
+ReviewStatus.find_or_create_by!(value: :system)
+ReviewStatus.find_or_create_by!(value: :reviewed)
+ReviewStatus.find_or_create_by!(value: :not_reviewed)
