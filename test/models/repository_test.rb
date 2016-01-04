78d68beb4391ac034fc36657389af10e47a262eb
RM Saksida <rm@saksida.com.br>
2016-01-04 19:43:28 -0200

    Add tests for `Repository`

---
+++ test/models/repository_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class RepositoryTest < ActiveSupport::TestCase
+  test '#search_index returns an ES index' do
+    khan = repositories(:khan)
+    idx = khan.search_index
+
+    assert_kind_of Search::Index, idx
+    assert_equal khan, idx.repository
+  end
+end
