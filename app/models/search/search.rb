c60714e84fb936c8526092326daeeb029c0b9e12
RM Saksida <rm@saksida.com.br>
2016-01-05 15:04:30 -0200

    Add a wrapper for search results

--- app/models/search/search.rb
+++ app/models/search/search.rb
@@ -5,6 +5,19 @@ module Search
 
     attr_accessor :indices
 
+    class Results
+      attr_reader :results, :hits
+
+      def initialize(results)
+        @results = results
+        @hits = results['hits']['hits']
+      end
+
+      def transform
+        Document.find(@hits.map { |h| h['_id'] })
+      end
+    end
+
     def initialize(*indices)
       self.indices = indices
     end
@@ -19,15 +32,20 @@ module Search
               should { match title: { query: q, operator: 'and', boost: 2 } }
               should { match description: { query: q, operator: 'and' } }
             end
+
           end
+        end
+
+        if options[:only].is_a? Array
+          fields options[:only]
         end
       end
 
-      client.search(
+      Search::Results.new(client.search(
         index: index_names,
         body: definition,
         type: 'document'
-      )
+      ))
     end
 
     def index_names
