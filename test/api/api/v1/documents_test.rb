39b17b727bc6c045a7dc12447dfa082b3d6b6281
RM Saksida <rm@saksida.com.br>
2016-01-07 09:45:43 -0200

    Add tests for searching docs with specific grades

--- test/api/api/v1/documents_test.rb
+++ test/api/api/v1/documents_test.rb
@@ -36,6 +36,27 @@ class API::V1::DocumentsTest < APITest
     assert_match (/algebra/i), last_json.first['description']
   end
 
+  test 'GET /api/documents/?grade_id= finds docs with specified grade' do
+    set_api_key
+    import_docs
+    grade = repositories(:api_docs).documents.first.grades.first
+
+    get '/api/v1/documents', grade_id: grade.id
+
+    assert_equal 1, last_json.size
+    assert last_json.first['grades'].any? { |g| g['id'] == grade.id }
+  end
+
+  test 'GET /api/documents/?grade_name= finds docs with specified grade' do
+    set_api_key
+    import_docs
+
+    get '/api/v1/documents', grade_name: 'grade 1'
+
+    assert_equal 1, last_json.size
+    assert last_json.first['grades'].any? { |g| g['name'] == 'Grade 1' }
+  end
+
   def import_docs
     delete_indices
 
