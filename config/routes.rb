e3f24f4c6b8d98bd4b2f0c6a748ca620c9c08077
RM Saksida <rm@saksida.com.br>
2016-01-16 18:33:11 -0200

    Reorganize visitor/authenticated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,30 @@
 Rails.application.routes.draw do
   mount API::V1::Root => '/api/v1'
 
-  resources :documents, only: [:index]
-
-  resources :document_exports, except: [:edit, :update] do
-    get :download, on: :member
-  end
-  
-  resources :document_imports, except: [:edit, :update] do
-    post :publish, on: :member
-  end
-  
-  resources :repositories
-
   devise_for :users
 
+  # Routes that explicitly ask for authentication.
+  # These routes prevent visitor routes from using the same URL.
+  authenticate :user do
+    resources :documents, only: [:index]
+
+    resources :document_exports, except: [:edit, :update] do
+      get :download, on: :member
+    end
+    
+    resources :document_imports, except: [:edit, :update] do
+      post :publish, on: :member
+    end
+    
+    resources :repositories
+  end
+
+  # Routes that are unavailable for visitors.
+  # These routes allow visitor routes using the same URL.
   authenticated :user do
     root 'welcome#index'
   end
 
+  # Public (visitor) routes.
   root 'visitor/welcome#index', as: :visitor_root
 end
