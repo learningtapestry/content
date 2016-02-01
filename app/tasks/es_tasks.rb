7acc82bcd38a8d409aecf12e4a54789bedfe6145
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-02-01 12:42:26 -0200

    utility tasks to handle index tasks

---
+++ app/tasks/es_tasks.rb
@@ -0,0 +1,24 @@
+module ESTasks
+  extend self
+
+  # Reset an index
+  # (caution: this will wipe out everything previously indexed)
+  def reset_index(model_name)
+    model = model_name.classify.constantize
+    index = model.new.search_index
+
+    index.reset_index!
+  end
+
+  # index all entries from the db
+  def index_all(model_name)
+    model = model_name.classify.constantize
+    index = model.new.search_index
+
+    if index.index_exists?
+      model.find_in_batches { |objs| index.bulk_index(objs) }
+    else
+      raise "Index does not exists #{index.index_name}"
+    end
+  end
+end
