2581749c28e6b1853dbd347532840dff45a91501
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-02-15 16:35:20 -0200

    add some comments to ccss task

--- app/tasks/ccss_tasks.rb
+++ app/tasks/ccss_tasks.rb
@@ -1,10 +1,7 @@
 module CcssTasks
   extend self
 
-  def subjects
-    ['ela', 'math']
-  end
-
+  # Load CCSS standards from json files
   def load
     subjects.each do |subject|
       file = File.open Rails.root.join('db', 'json', "ccss_#{subject}.json")
@@ -13,25 +10,33 @@ module CcssTasks
     end
   end
 
-  def parse_tree(root)
-    is_leaf = root.leaf == 'true'
-    if is_leaf
-      data = extract_data root
+  # the CCSS subjects
+  def subjects
+    ['ela', 'math']
+  end
+
+  def is_leaf?(node)
+    node.leaf == 'true'
+  end
+
+  def parse_tree(node)
+    if is_leaf?(node)
+      data = extract_data node
       save_standard data
     end
-    root.fetch(:children, []).each { |node| parse_tree(node) }
+    node.fetch(:children, []).each { |new_node| parse_tree(new_node) }
   end
 
   def extract_data(h)
     {
       name: h.asn_statementNotation,
       description: h.text,
-      url: h.skos_exactMatch.try(:first).try(:uri) || h.id,
+      url: h.skos_exactMatch.try(:first).try(:uri) || h.id,  # try to get corestandards.org URI first, else uses jesandco URI
       definitions: [
-        h.asn_statementNotation,
-        h.asn_altStatementNotation,
-        h.id.split('/').last,
-        h.skos_exactMatch.try(:last).try(:uri)
+        h.asn_statementNotation,                # full identifier
+        h.asn_altStatementNotation,             # simplified identifier
+        h.id.split('/').last,                   # jesandco uri id
+        h.skos_exactMatch.try(:last).try(:uri)  # urn:guid
       ].compact,
     }
   end
