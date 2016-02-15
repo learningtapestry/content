b3627c1b1ccd5fd8ab227b8149eccd1d40562aa2
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-02-15 09:18:04 -0200

    save standards json from jesandco.org

---
+++ scripts/fetch_standards_json.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env sh
+
+# ELA:
+wget -O ../db/json/ela.json http://asn.jesandco.org/sites/default/files/ASN/manifest/D10003FC.json
+
+# MATH:
+wget -O ../db/json/math.json http://asn.jesandco.org/sites/default/files/ASN/manifest/D10003FB.json
